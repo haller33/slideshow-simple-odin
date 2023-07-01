61a5b59b28005a483570d96522c13bcac057bb5e
haller33 <eliseu42lucena@gmail.com>
2023-07-01 17:17:43 -0300

    remove unecessary changes and edit readme

--- build.sh
+++ build.sh
@@ -5,18 +5,13 @@ set -x
 
 if [ $# -eq 2 ]; then
 
-    odin build src -out:spawn_rune.bin -o:speed -reloc-mode:static && echo "OK"
+    odin build src -out:slideshow-simple.bin -o:speed -reloc-mode:static && echo "OK"
 
 elif [ $# -eq 1 ]; then
-    clang -std=c89 -g -D_DEFAULT_SOURCE -O3 \
-        -c ./lib/readdir_files/creaddir_files.c -o ./lib/creaddir_files.a
 
-    odin build src -debug -out:spawn_rune.bin && echo "OK"
+    odin build src -debug -out:slideshow-simple.bin && echo "OK"
 else
 
-    clang -std=c89 -D_DEFAULT_SOURCE -O3 \
-        -c ./lib/readdir_files/creaddir_files.c -o ./lib/creaddir_files.a
-
-    odin build src -out:spawn_rune.bin && echo "OK"
+    odin build src -out:slideshow-simple.bin && echo "OK"
 
 fi
