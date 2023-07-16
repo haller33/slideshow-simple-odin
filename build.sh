bdc0e03b6cf56c16e79462ffd9de816d35d97af9
haller33 <eliseu42lucena@gmail.com>
2023-07-16 09:24:47 -0300

    new build on /bin folder

--- build.sh
+++ build.sh
@@ -2,16 +2,17 @@
 
 set -x
 
+mkdir -p bin
 
 if [ $# -eq 2 ]; then
 
-    odin build src -out:slideshow-simple.bin -o:speed -reloc-mode:static && echo "OK"
+    odin build src -out:bin/slideshow-simple.bin -o:speed -reloc-mode:static && echo "OK"
 
 elif [ $# -eq 1 ]; then
 
-    odin build src -debug -out:slideshow-simple.bin && echo "OK"
+    odin build src -debug -out:bin/slideshow-simple.bin && echo "OK"
 else
 
-    odin build src -out:slideshow-simple.bin && echo "OK"
+    odin build src -out:bin/slideshow-simple.bin && echo "OK"
 
 fi
